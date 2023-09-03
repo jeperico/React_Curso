848b58d3a616161e908784942f18f12811c4099b
jeperico <jeperico@terra.com.br>
2023-09-02 23:37:54 -0300

    Form

--- client/src/components/EventClicker/Form.jsx
+++ client/src/components/EventClicker/Form.jsx
@@ -1,18 +1,40 @@
 import '../DivsContainer.css'
+import { useState } from 'react'
 
 function Form(){
 
     function cadastrarUsuario(e){
         e.preventDefault()
+        console.log(`Nome do usuario: ${name}  Senha: ${password}`)
         console.log('Usuario cadastrado')
     }
+
+    const [name, setName] = useState('Matheus')
+    const [password, setPassword] = useState('123milhas')
 
     return(
         <div className="DivContainer">
             <h1>Formulário</h1>
             <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type="text" placeholder='Digite seu nome' />
+                    <label htmlFor="name">Nome: </label>
+                    <input
+                      type="text"
+                      id="name"
+                      name="name"
+                      placeholder='Digite seu nome'
+                      onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Senha: </label>
+                    <input
+                      type="password"
+                      id="password"
+                      name="password"
+                      placeholder='Digite a sua senha'
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
                 </div>
                 <div>
                     <input type="submit" value="Cadastrar" />
