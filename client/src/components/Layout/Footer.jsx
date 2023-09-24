fc393fbb33d7768e00839b1ec6129b1c38ef7f47
jeperico <jeperico@terra.com.br>
2023-09-24 19:55:58 -0300

    Fim do Curso
    Acabou as aulas do Curso, agora vem os projetos

--- client/src/components/Layout/Footer.jsx
+++ client/src/components/Layout/Footer.jsx
@@ -1,8 +1,21 @@
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
+
 function Footer(){
 
     return(
         <footer>
-            Rodapé
+            <ul>
+                <li>
+                    <FaFacebook />
+                </li>
+                <li>
+                    <FaInstagram />
+                </li>
+                <li>
+                    <FaLinkedin />
+                </li>
+            </ul>
+            <p>Nosso Rodapé</p>
         </footer>
     )
 }
--- client/src/components/Layout/NavBar.jsx
+++ client/src/components/Layout/NavBar.jsx
@@ -6,15 +6,9 @@ function NavBar(){
 
     return(
         <nav className='list'>
-            <ul className='list'>
-                <li className='item'><Link to="/">Home</Link></li>
-            </ul>
-            <ul className='list'>
-                <li className='item'><Link to="/empresa">Empresa</Link></li>
-            </ul>
-            <ul className='list'>
-                <li className='item'><Link to="/contate">Contate</Link></li>
-            </ul>
+            <li className='item'><Link to="/">Home</Link></li>
+            <li className='item'><Link to="/empresa">Empresa</Link></li>
+            <li className='item'><Link to="/contate">Contate</Link></li>
         </nav>
     )
 }
